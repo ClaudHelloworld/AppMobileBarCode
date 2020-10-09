558311ac37dee487676bb71c8806435460684f1c
ClaudHelloworld <fantasmagomes999@gmail.com>
2020-10-09 11:07:14 +0900

    Atualização, add os produtos

---
+++ www/components/action.js
@@ -0,0 +1,23 @@
+$(document).on("click",'#somar',function(){
+  
+
+     var camera = ("#camera").val();
+
+
+if(camera == '280720550'){
+
+}
+else if(camera == '989895555'){
+
+}
+else if(camera == '85236987'){
+
+}
+else if(camera == '85369877444'){
+
+}
+ 
+    $("#imagem").text(iamgem.toFixed(2));
+    console.log(camera);
+});
+
