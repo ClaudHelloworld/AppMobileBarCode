4336cd5c0d04c9ef8f54b61b355e6b076c3d5a95
ClaudHelloworld <fantasmagomes999@gmail.com>
2020-10-09 11:12:28 +0900

    Finaltry

--- www/components/action.js
+++ www/components/action.js
@@ -5,16 +5,16 @@ $(document).on("click",'#somar',function(){
 
 
 if(camera == '280720550'){
-
+<a href="lib/sites/p1.html">Produto1</a>
 }
 else if(camera == '989895555'){
-
+<a href="lib/sites/p2.html">Produto2</a>
 }
 else if(camera == '85236987'){
-
+<a href="lib/sites/p3.html">Produto3</a>
 }
 else if(camera == '85369877444'){
-
+<a href="lib/sites/p4.html">Produto4</a>
 }
  
     $("#imagem").text(iamgem.toFixed(2));
